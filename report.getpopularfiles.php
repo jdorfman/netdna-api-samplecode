c945b6509147e2a4de78b74bd5c74f97e19f6bf6
Justin Dorfman <justindorfman@gmail.com>
2011-05-19 15:26:17 -0700

    report.get*

---
+++ report.getpopularfiles.php
@@ -0,0 +1,28 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    include("lib/xmlrpc.inc");
+    $cur = date('c');
+    $apiKey = 'api-key';
+    $apiUserId = 'user-id';
+    $namespace = 'report';
+    $method = 'getPopularFiles';
+    $authString = hash('sha256', $cur . ':' . $apiKey . ':' . $method);
+    $companyId = 'acmeinc';
+    $dateFrom = '2011-05-10';
+    $dateTo = '2011-05-12';
+    $zoneId = 'zoneid';
+
+    $f=new xmlrpcmsg("$namespace.$method", array(
+    	php_xmlrpc_encode($apiUserId),
+    	php_xmlrpc_encode($authString),
+    	php_xmlrpc_encode($cur),
+    	php_xmlrpc_encode($companyId),
+    	php_xmlrpc_encode($dateFrom),
+    	php_xmlrpc_encode($dateTo),
+    	php_xmlrpc_encode($zoneId),
+    						));
+
+    $c=new xmlrpc_client("/xmlrpc/report", "api.netdna.com", 80,'http11');
+    $r=&$c->send($f);
+    print_r($r);
+?>
---
+++ report.gettotalhits.php
@@ -0,0 +1,24 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    include("lib/xmlrpc.inc");
+    $cur = date('c');
+    $apiKey = 'api-key';
+    $apiUserId = 'user-id';
+    $namespace = 'report';
+    $method = 'getTotalHits';
+    $authString = hash('sha256', $cur . ':' . $apiKey . ':' . $method);
+    $zoneId = 'zoneid';
+    $type = '1'; //1=today, 2=current hour, 3=date range
+
+    $f=new xmlrpcmsg("$namespace.$method", array(
+    php_xmlrpc_encode($apiUserId),
+    php_xmlrpc_encode($authString),
+    php_xmlrpc_encode($cur),
+    php_xmlrpc_encode($zoneId),
+    php_xmlrpc_encode($type),
+    ));
+
+    $c=new xmlrpc_client("/xmlrpc/report", "api.netdna.com", 80,'http11');
+    $r=&$c->send($f);
+    print_r($r);
+?>
