16aa3cf086f1e447faa3207d485716f02fafb00c
Justin Dorfman <justindorfman@gmail.com>
2012-02-14 10:55:29 -0800

    pullzine.cachepurge 2012

--- pullzone.cachepurge.php
+++ pullzone.cachepurge.php
@@ -1,15 +1,12 @@
 <?php
-date_default_timezone_set('America/Los_Angeles');
 include("lib/xmlrpc.inc");
-$cur = date('c');
-$apiKey = 'api-key';
-$apiUserId = 'user-id';
+include("lib/config.inc");
 $namespace = 'cache';
 $method = 'purge';
 $authString = hash('sha256', $cur . ':' . $apiKey . ':' . $method);
 
 // this is the url to purge
-$url= 'http://static.example.com/static/images/logo.png';
+$url= 'http://cdn.86400.io/beta03/app/files/NetDNA-logo-withtagline-wide.png';
 $f=new xmlrpcmsg("$namespace.$method", array(php_xmlrpc_encode($apiUserId),
 php_xmlrpc_encode($authString), php_xmlrpc_encode($cur),
 php_xmlrpc_encode($url)));
