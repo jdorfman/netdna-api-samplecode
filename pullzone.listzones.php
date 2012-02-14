7e4071512367768eb51507848412f6b93c5ce003
Justin Dorfman <justindorfman@gmail.com>
2012-02-14 11:00:46 -0800

    pullzone.listzones 2012

--- pullzone.listzones.php
+++ pullzone.listzones.php
@@ -1,20 +1,16 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
 include("lib/xmlrpc.inc");
-$cur = date('c');
-$apiKey = 'api-key';
-$apiUserId = 'user-id';
+include("lib/config.inc");
 $namespace = 'pullzone';
 $method = 'listZones';
 $authString = hash('sha256', $cur . ':' . $apiKey . ':' . $method);
-$zoneid = 'zone-id';
 $type = '1';
 
 	$f=new xmlrpcmsg("$namespace.$method", array(
 		php_xmlrpc_encode($apiUserId),
 		php_xmlrpc_encode($authString), 
 		php_xmlrpc_encode($cur), 
-		php_xmlrpc_encode($zoneid),
 		php_xmlrpc_encode($type),
 	));
 
