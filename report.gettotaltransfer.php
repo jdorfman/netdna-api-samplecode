007b6b4946ab154b48a75cd01901d21c1568971a
Justin Dorfman <justindorfman@gmail.com>
2012-02-15 10:20:51 -0800

    report.gettotaltransfer 2012

--- report.gettotaltransfer.php
+++ report.gettotaltransfer.php
@@ -1,14 +1,13 @@
 <?php
-    date_default_timezone_set('America/Los_Angeles');
     include("lib/xmlrpc.inc");
-    $cur = date('c');
-    $apiKey = 'api-key';
-    $apiUserId = 'user-id';
+    include("lib/config.inc");
     $namespace = 'report';
     $method = 'getTotalTransfer';
     $authString = hash('sha256', $cur . ':' . $apiKey . ':' . $method);
-    $zoneId = 'zoneid';
-    $type = '1'; //1=today, 2=current hour, 3=date range
+    $zoneId = '21826';
+    $type = '2'; //1=today, 2=current hour, 3=date range
+    $dateFrom = '2012-02-01';
+    $dateTo = '2012-02-14';
 
     $f=new xmlrpcmsg("$namespace.$method", array(
     php_xmlrpc_encode($apiUserId),
@@ -16,6 +15,8 @@
     php_xmlrpc_encode($cur),
     php_xmlrpc_encode($zoneId),
     php_xmlrpc_encode($type),
+    php_xmlrpc_encode($dateFrom),
+    php_xmlrpc_encode($dateTo),
     ));
 
     $c=new xmlrpc_client("/xmlrpc/report", "api.netdna.com", 80,'http11');
